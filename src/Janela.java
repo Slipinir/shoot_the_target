9a9801baa7f53c84650c5219b8975e3eeb2f83c1
Luiz Carlos Mendonça Jr <quilinho.junior@gmail.com>
2016-03-31 02:37:44 -0300

    Instantiated the window

--- src/Janela.java
+++ src/Janela.java
@@ -1,2 +1,9 @@
-public class Janela {
+public class Janela extends BaseTutorial {
+
+    public void init() {
+    }
+
+    public static void main(String[] args) {
+        new Start(new Janela());
+    }
 }
