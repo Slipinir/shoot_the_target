55d44cd08e306d05f5dde2e9f23325627c7e30b1
Luiz Carlos Mendonça Jr <quilinho.junior@gmail.com>
2016-04-01 01:51:21 -0300

    Tiro image, Janela and Tiro beggining

--- src/Janela.java
+++ src/Janela.java
@@ -1,6 +1,13 @@
+import java.awt.Graphics;
+
 public class Janela extends BaseTutorial {
 
     public void init() {
+    }
+
+    public void paint(Graphics g) {
+        if (keyPressed && keyCode == 32)
+        g.drawImage(Tiro.getTiro(), 0, 0, this);
     }
 
     public static void main(String[] args) {
--- src/Tiro.java
+++ src/Tiro.java
@@ -1,2 +1,19 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Tiro {
+    private static BufferedImage tiro;
+
+    public static BufferedImage getTiro() {
+        try {
+            if (tiro == null)
+                tiro = ImageIO.read(new File("../img/tiro.png"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return tiro;
+    }
 }
