fac61cb63c453da32c45527e6bb42314637f2e3e
Luiz Carlos Mendonça Jr <quilinho.junior@gmail.com>
2016-04-02 00:18:49 -0300

    Simplified the project

--- src/Janela.java
+++ src/Janela.java
@@ -1,13 +1,70 @@
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Janela extends BaseTutorial {
 
+    private BufferedImage missel;
+
+    private BufferedImage alvo;
+    
+    // Coordenadas do missel
+    private int misselX, misselY;
+
+    private int alvoX, alvoY;
+
+    // Executa apenas uma vez, quando inicia a janela
     public void init() {
+        try {
+            // Cria a imagem
+            missel = ImageIO.read(new File("../img/tiro.png"));
+            // Coordenadas do missel
+            misselX = misselY = 0;
+            // Coordenadas do alvo (alterar para ficar centralizado na tela)
+            alvoX = alvoY = 0;
+            // Cria o alvo
+            alvo = ImageIO.read(new File("../img/tiro.png"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void paint(Graphics g) {
-        if (keyPressed && keyCode == 32)
-        g.drawImage(Tiro.getTiro(), 0, 0, this);
+        // Verifica se alguma tecla foi pressionada
+        if (keyPressed) { 
+            // Verifica qual tecla foi pressionada
+            // Se foi para cima, diminui o y para mover o missel para cima
+            // Se foi para baixo, aumenta o y para mover o missel para baixo
+            // Adapte a quantidade que se movimenta de acordo com a necessidade
+            // E etc
+            switch (keyCode) {
+                case 38: 
+                    misselY -= 10;
+                    break;
+                case 40:
+                    misselY += 10;
+                    break;
+                case 37:
+                    misselX -= 10;
+                    break;
+                case 39:
+                    misselX += 10;
+                    break;
+            }
+        }
+        // Mostra o missel na tela, de acordo com as variaveis x e y
+        g.drawImage(missel, misselX, misselY, this);
+
+        // Mostra o alvo sempre no mesmo lugar
+        g.drawImage(alvo, alvoX, alvoY, this);
+
+        // Aqui verifica se o missel entrou em contato com o alvo
+        //if (mise) {
+        //}
+        // Se entrar em contato, finalize a tela (veja os exemplos que o professor deu)
+        // Mostre alguma mensagem que o jogador conseguiu atingir o alvo
     }
 
     public static void main(String[] args) {
