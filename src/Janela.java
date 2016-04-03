b1bab91e73d0b2c5219fb066b09f5dffed0595a9
bmy1991 <bmy1991@hotmail.com>
2016-04-03 14:38:41 -0300

    Alterações Diversas
    Jogo Terminado

--- src/Janela.java
+++ src/Janela.java
@@ -3,34 +3,43 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.awt.Rectangle;
+import javax.swing.JOptionPane;
 
 public class Janela extends BaseTutorial {
-
+	
     private BufferedImage missel;
-
     private BufferedImage alvo;
     
-    // Coordenadas do missel
+    // Retângulos utilizados para detecção de colisão
+    private Rectangle r1;
+    private Rectangle r2;
+    
+    // Coordenadas gerais (Missel, Alvo, e Retângulos)
     private int misselX, misselY;
-
     private int alvoX, alvoY;
+    private int r1x, r1y;
 
     // Executa apenas uma vez, quando inicia a janela
     public void init() {
         try {
+			r1 = new Rectangle(128,128);
+			r2 = new Rectangle(700, 0, 128,128);
             // Cria a imagem
-            missel = ImageIO.read(new File("../img/tiro.png"));
+            missel = ImageIO.read(new File("../img/missile.png"));
             // Coordenadas do missel
             misselX = misselY = 0;
             // Coordenadas do alvo (alterar para ficar centralizado na tela)
             alvoX = alvoY = 0;
             // Cria o alvo
-            alvo = ImageIO.read(new File("../img/tiro.png"));
+            alvo = ImageIO.read(new File("../img/bullseye.png"));
+            // Coordenadas dos retângulos
+            r1x = r1y = 0;
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-
+	
     public void paint(Graphics g) {
         // Verifica se alguma tecla foi pressionada
         if (keyPressed) { 
@@ -42,32 +51,44 @@ public class Janela extends BaseTutorial {
             switch (keyCode) {
                 case 38: 
                     misselY -= 10;
+                    r1y -= 10;
                     break;
                 case 40:
                     misselY += 10;
+                    r1y += 10;
                     break;
                 case 37:
                     misselX -= 10;
+                    r1x -= 10;
                     break;
                 case 39:
                     misselX += 10;
+                    r1x += 10;
                     break;
             }
         }
         // Mostra o missel na tela, de acordo com as variaveis x e y
         g.drawImage(missel, misselX, misselY, this);
-
+		
         // Mostra o alvo sempre no mesmo lugar
-        g.drawImage(alvo, alvoX, alvoY, this);
+        // Para mostrar o alvo no meio do lado direito, x = 672 e y = (height/2)-80
+        g.drawImage(alvo, 672, 0, this);
+        
+        r1.setLocation(r1x, r1y);
 
         // Aqui verifica se o missel entrou em contato com o alvo
-        //if (mise) {
-        //}
+        
+        if(r1.intersects(r2)) {
+			JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+			System.exit(ABORT);
+        }
+        
         // Se entrar em contato, finalize a tela (veja os exemplos que o professor deu)
         // Mostre alguma mensagem que o jogador conseguiu atingir o alvo
     }
 
     public static void main(String[] args) {
-        new Start(new Janela());
+		Start start = new Start(new Janela());
+		start.setSize(800,200);
     }
 }
